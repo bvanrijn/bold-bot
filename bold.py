0c577c49713eadf9f102b4d1bfaed602bc6f13d2
bvanrijn <bvanrijn@users.noreply.github.com>
2016-06-19 15:21:03 +0200

    Update bold.py

--- bold.py
+++ bold.py
@@ -3,22 +3,9 @@
 #
 # Simple Bot to reply to Telegram messages
 # This program is dedicated to the public domain under the CC0 license.
-"""
-This Bot uses the Updater class to handle the bot.
 
-First, a few handler functions are defined. Then, those functions are passed to
-the Dispatcher and registered at their respective places.
-Then, the bot is started and runs until we press Ctrl-C on the command line.
-
-Usage:
-Basic inline bot example. Applies different text transformations.
-Press Ctrl-C on the command line or send a signal to the process to stop the
-bot.
-"""
 from uuid import uuid4
-
 import re
-
 from telegram import InlineQueryResultArticle, ParseMode, \
     InputTextMessageContent
 from telegram.ext import Updater, InlineQueryHandler, CommandHandler
@@ -30,22 +17,18 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 logger = logging.getLogger(__name__)
 
-
 # Define a few command handlers. These usually take the two arguments bot and
 # update. Error handlers also receive the raised TelegramError object in error.
 def start(bot, update):
     bot.sendMessage(update.message.chat_id, text='Hi! I\'m an inline bot. You can use me in all of your chats. Just type @bold_oss_bot and then type your message, then click or tap one of the 3 styles to send the message.')
 
-
 def help(bot, update):
     bot.sendMessage(update.message.chat_id, text='You can choose from 3 different styles: bold, italic and fixedsys. Type your message and then tap any of the styles to send your message. Yes, I know I do the same as @bold, but I\'m open source! You can see my code at https://github.com/bvanrijn/bold-bot')
-
 
 def escape_markdown(text):
     """Helper function to escape telegram markup symbols"""
     escape_chars = '\*_`\['
     return re.sub(r'([%s])' % escape_chars, r'\\\1', text)
-
 
 def inlinequery(bot, update):
     query = update.inline_query.query
@@ -71,10 +54,8 @@ def inlinequery(bot, update):
 
     bot.answerInlineQuery(update.inline_query.id, results=results)
 
-
 def error(bot, update, error):
     logger.warn('Update "%s" caused error "%s"' % (update, error))
-
 
 def main():
     # Create the Updater and pass it your bot's token.
@@ -100,7 +81,6 @@ def main():
     # SIGTERM or SIGABRT. This should be used most of the time, since
     # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
-
 
 if __name__ == '__main__':
     main()
